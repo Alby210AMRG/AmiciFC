a6cc1545b1549cf928f77100415bb4d804baabfb
Alby <alberto.rossi.0112@gmail.com>
2026-06-16 15:49:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='amicifc-v1552';
+const CACHE='amicifc-v1553';
 const ASSETS=['amici-fc.html','images/match-default.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
